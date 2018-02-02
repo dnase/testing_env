fc8692a59ce29313a3dd02052b79c04470b28028
Drew Nase <dnasecs@gmail.com>
2018-02-02 18:44:56 -0500

    add some forwarded ports and update dependencies

--- Vagrantfile
+++ Vagrantfile
@@ -4,6 +4,8 @@
 Vagrant.configure("2") do |config|
   config.vm.box = "ubuntu/trusty64"
 
+  config.vm.network :forwarded_port, guest: 59008, host: 59008, id: "drumpf", auto_correct: true
+
   config.vm.provider "virtualbox" do |v, override|
       v.customize ["modifyvm", :id, "--memory", 8192]
       v.customize ["modifyvm", :id, "--cpus", 4]
--- files/deps.sh
+++ files/deps.sh
@@ -1,8 +1,15 @@
-apt-get -y install git
-apt-get -y install build-essential libtool automake autotools-dev autoconf pkg-config libssl-dev libgmp3-dev libevent-dev bsdmainutils
-apt-get -y install libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-program-options-dev libboost-test-dev libboost-thread-dev
-apt-get -y install libboost-all-dev
 add-apt-repository -y ppa:bitcoin/bitcoin
 apt-get update
-apt-get install -y libdb4.8-dev libdb4.8++-dev
+# general dependencies
+apt-get -y install git
+apt-get -y install build-essential libtool automake autotools-dev autoconf pkg-config libssl-dev libgmp3-dev libevent-dev bsdmainutils libc6-dev-i386
+apt-get -y install libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-program-options-dev libboost-test-dev libboost-thread-dev
+apt-get -y install libboost-all-dev
+apt-get install -y libdb6.0-dev libdb6.0++-dev
 apt-get install -y libminiupnpc-dev libzmq3-dev
+# dependencies for compiling on Windows
+apt-get install -y g++-mingw-w64-i686 mingw-w64-i686-dev g++-mingw-w64-x86-64 mingw-w64-x86-64-dev
+apt-get install -y qtcreator qttools5-dev-tools
+git clone https://github.com/DrumpfCore/Drumpfcoin.git /root/drumpfcode
+cd /root/drumpfcode
+git checkout drumpf2
