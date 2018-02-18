81c07c838339a73944cd8e285da39e04f61c1f6d
Drew Nase <dnasecs@gmail.com>
2018-02-18 16:19:21 -0500

    fixing versions

--- files/scripts/bootstrap_docker.sh
+++ files/scripts/bootstrap_docker.sh
@@ -1,7 +1,7 @@
 apt-get update
 apt-get install -y docker.io python python-pip
 docker build -t basebox:latest /vagrant/files/containers/base
-pip install docker==2.7.0
+pip install docker
 pip install python-daemon
 cp /vagrant/files/amms/ammsd.py /usr/local/bin/ammsd
 cp /vagrant/files/amms/build_images.py /usr/local/bin/build_images
